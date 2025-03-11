9cc8dccb10fd86e8db4d73db0b06cf2e1a86708f
zL4mpY <LampNotLama@yandex.ru>
2025-03-12 00:27:27 +0300

    one more fix i hope this works

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,8 @@
 
 using namespace geode::prelude;
 
-#if (defined(GEODE_IS_WINDOWS) || defined(GEODE_IS_MACOS))
 #include <geode.custom-keybinds/include/Keybinds.hpp>
 using namespace keybinds;
-#endif
 
 class $modify(MyEditorUI, EditorUI) {
 	static void onModify(auto& self) {
@@ -27,8 +25,6 @@ class $modify(MyEditorUI, EditorUI) {
 		if (!EditorUI::init(editorLayer)) {
 			return false;
 		}
-
-		#if (defined(GEODE_IS_WINDOWS) || defined(GEODE_IS_MACOS))
 			
 		this->template addEventListener<InvokeBindFilter>([=](InvokeBindEvent* event) {
 			if (event->isDown()) {
@@ -57,8 +53,6 @@ class $modify(MyEditorUI, EditorUI) {
 			}
 			return ListenerResult::Propagate;
 		}, "clone-and-move-obj-right"_spr);
-
-		#endif
 
 		// Creating an array of required values for buttons
 		const std::array<float, 4> rotations = {0, 180, 270, 90};
